49eb5acd6f8863bfb02f4829f4c38b19bc091b7b
Hazal Ozturk <hcanisag@gmail.com>
2018-01-03 19:00:10 -0500

    Fix initial state on searched books

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ class Book extends Component {
             <div className="book-top">
               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
               <div className="book-shelf-changer">
-                <select value={book.shelf} onChange={(event) => this.props.onChangeShelf(book, event.target.value)}>
+                <select value={book.shelf || "none"} onChange={(event) => this.props.onChangeShelf(book, event.target.value)}>
                   <option value="none" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
